8e8ede8b2065a32b7a65975aaff4b0f224fee3df
Roderick Kar <rod@rodkar.com>
2015-03-22 23:26:35 +0800

    Update from 13

--- app/src/main/java/com/cloudyun/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/cloudyun/stormy/ui/DailyForecastActivity.java
@@ -1,11 +1,15 @@
 package com.cloudyun.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 import com.cloudyun.stormy.R;
 import com.cloudyun.stormy.adapters.DayAdapter;
 import com.cloudyun.stormy.weather.Day;
+
+import java.util.Arrays;
 
 public class DailyForecastActivity extends ListActivity {
 
@@ -19,6 +23,12 @@ public class DailyForecastActivity extends ListActivity {
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
 //        setListAdapter(adapter);
 
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+
         DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
     }
 }
--- app/src/main/java/com/cloudyun/stormy/ui/MainActivity.java
+++ app/src/main/java/com/cloudyun/stormy/ui/MainActivity.java
@@ -39,6 +39,8 @@ import butterknife.OnClick;
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String DAILY_FORECAST = "DAILY_FORECAST";
+    public static final String HOURLY_FORECAST = "HOURLY_FORECAST";
 
     private Forecast mForecast;
 
@@ -259,6 +261,14 @@ public class MainActivity extends ActionBarActivity {
     @OnClick (R.id.dailyButton)
     public void startDailyActivity(View view){
         Intent intent = new Intent(this, DailyForecastActivity.class);
+        intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
+        startActivity(intent);
+    }
+
+    @OnClick (R.id.hourlyButton)
+    public void startHourlyActivity(View view){
+        Intent intent = new Intent(this, HourlyForecastActivity.class);
+        intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
         startActivity(intent);
     }
 }
