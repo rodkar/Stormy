e31c68a52fe0915c443ec5aac3fcd90bb5108c79
Roderick Kar <rod@rodkar.com>
2015-03-25 15:20:53 +0800

    Update from 13

--- app/src/main/java/com/cloudyun/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/cloudyun/stormy/ui/DailyForecastActivity.java
@@ -4,6 +4,9 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.cloudyun.stormy.R;
 import com.cloudyun.stormy.adapters.DayAdapter;
@@ -30,5 +33,19 @@ public class DailyForecastActivity extends ListActivity {
 
         DayAdapter adapter = new DayAdapter(this, mDays);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+
+        String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, highTemp, conditions);
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
     }
 }
